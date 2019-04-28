8fcf7197d0246e52000884c40b416e3d61656a98
Mhrpatel85 <29385964+Mhr85@users.noreply.github.com>
2019-04-28 13:44:57 -0400

    added start button

---
+++ game.js
@@ -0,0 +1,100 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+//console.log(buttonColours)
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$("#start-game").click(function() {
+  animatePress();
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColor = $(this).attr("id");
+  console.log(userChosenColor);
+  userClickedPattern.push(userChosenColor);
+  console.log(userClickedPattern)
+
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+
+  checkAnswer(userClickedPattern.length-1)
+})
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("Wrong");
+
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game over press any key to restart");
+
+    startOver();
+  }
+}
+
+
+
+function nextSequence() {
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  //console.log(randomNumber);
+  var randomChosenColor = buttonColours[randomNumber];
+  //console.log("This is the randomly selected color: " + randomChosenColor)
+  gamePattern.push(randomChosenColor)
+
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColor);
+
+
+}
+//nextSequence();
+
+// this function takes a single input parameter called name
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+//1. Create a new function called animatePress(), it should take a single input parameter called currentColour.
+function animatePress(currentColor) {
+
+  //2. Use jQuery to add this pressed class to the button that gets clicked inside animatePress().
+  $("#" + currentColor).addClass("pressed");
+
+  //3. use Google/Stackoverflow to figure out how you can use Javascript to remove the pressed class after a 100 milliseconds.
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
