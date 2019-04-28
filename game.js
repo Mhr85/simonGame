9747c4f5f6030f1f421f4782251ce267ca6672c1
Mhrpatel85 <29385964+Mhr85@users.noreply.github.com>
2019-04-28 13:50:48 -0400

    updated game over message

--- game.js
+++ game.js
@@ -46,7 +46,7 @@ function checkAnswer(currentLevel) {
       $("body").removeClass("game-over");
     }, 200);
 
-    $("#level-title").text("Game over press any key to restart");
+    $("#level-title").text("Game over");
 
     startOver();
   }
